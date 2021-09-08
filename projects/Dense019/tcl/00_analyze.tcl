ac419f7dba6824bc1ccbea14f58f586b8310d8b5
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2021-09-08 16:34:51 +0200

    Change hls4ml-catapult-template to cpp-src in Dense019

--- projects/Dense019/tcl/00_analyze.tcl
+++ projects/Dense019/tcl/00_analyze.tcl
@@ -4,5 +4,5 @@
 options set Input/CppStandard c++11
 options set Input/TargetPlatform x86_64
 solution file add ./myproject.cpp
-solution file set ./myproject.cpp -args "-I${MAIN_PATH}/hls4ml-catapult-template/catapult -I${PROJECT_PATH}/activation_tables"
+solution file set ./myproject.cpp -args "-I${MAIN_PATH}/cpp-src/catapult -I${PROJECT_PATH}/activation_tables"
 if {$RUN_TEST == 1 || $COMPILE_ONLY == 0} { solution file add ./myproject_test.cpp -exclude true }
