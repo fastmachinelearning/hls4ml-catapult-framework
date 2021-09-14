7c881026eb420e8e1ed88b62a67a78ae4d720f34
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2021-09-14 10:24:08 +0200

    Add documentation to 04_architect.tcl in Dense019

--- projects/Dense019/tcl/04_architect.tcl
+++ projects/Dense019/tcl/04_architect.tcl
@@ -1,40 +1,62 @@
 # Put here the code to be executed immediately before running 'go architect'
 # in Catapult.
 
-directive set /myproject/layer1/core -DESIGN_GOAL Area
-directive set /myproject/layer4/core -DESIGN_GOAL Area
-directive set /myproject/layer3/core -DESIGN_GOAL Area
-directive set /myproject/layer2/core -DESIGN_GOAL Area
 
-directive set /myproject/layer1/core/Product1 -UNROLL yes
-directive set /myproject/layer1/core/Product2 -UNROLL yes
-directive set /myproject/layer1/core/Accum1 -UNROLL yes
-directive set /myproject/layer1/core/Accum2 -UNROLL yes
-directive set /myproject/layer1/core/Result -UNROLL yes
+# Uncomment the region below (*) and comment the region afterwards (**) if you
+# want to synthesise Dense019 as a connection of blocks. You can set IIs/RFs
+# through PIPELINE_INIT_INTERVAL (see the comment inside the (*) region).
 
-directive set /myproject/layer2/core/Relu -UNROLL yes
 
-directive set /myproject/layer3/core/Product1 -UNROLL yes
-directive set /myproject/layer3/core/Product2 -UNROLL yes
-directive set /myproject/layer3/core/Accum1 -UNROLL yes
-directive set /myproject/layer3/core/Accum2 -UNROLL yes
-directive set /myproject/layer3/core/Result -UNROLL yes
+### Region (*) starts
+# directive set /myproject/layer1/core -DESIGN_GOAL Area
+# directive set /myproject/layer2/core -DESIGN_GOAL Area
+# directive set /myproject/layer3/core -DESIGN_GOAL Area
+# directive set /myproject/layer4/core -DESIGN_GOAL Area
 
-directive set /myproject/layer4/core/SoftmaxLatencyExp -UNROLL yes
-directive set /myproject/layer4/core/SoftmaxLatencyCalc -UNROLL yes
+# directive set /myproject/layer1/core/Product1 -UNROLL yes
+# directive set /myproject/layer1/core/Product2 -UNROLL yes
+# directive set /myproject/layer1/core/Accum1 -UNROLL yes
+# directive set /myproject/layer1/core/Accum2 -UNROLL yes
+# directive set /myproject/layer1/core/Result -UNROLL yes
 
-directive set /myproject/layer1/core/main -PIPELINE_INIT_INTERVAL 1
-directive set /myproject/layer2/core/main -PIPELINE_INIT_INTERVAL 1
-directive set /myproject/layer3/core/main -PIPELINE_INIT_INTERVAL 1
-directive set /myproject/layer4/core/main -PIPELINE_INIT_INTERVAL 1
+# directive set /myproject/layer2/core/Relu -UNROLL yes
 
-directive set /myproject/layer2_out:cns -FIFO_DEPTH 0
-directive set /myproject/layer3_out:cns -FIFO_DEPTH 0
-directive set /myproject/layer4_out:cns -FIFO_DEPTH 0
+# directive set /myproject/layer3/core/Product1 -UNROLL yes
+# directive set /myproject/layer3/core/Product2 -UNROLL yes
+# directive set /myproject/layer3/core/Accum1 -UNROLL yes
+# directive set /myproject/layer3/core/Accum2 -UNROLL yes
+# directive set /myproject/layer3/core/Result -UNROLL yes
 
-directive set /myproject/layer4/core/main -PIPELINE_STALL_MODE bubble
+# directive set /myproject/layer4/core/SoftmaxLatencyExp -UNROLL yes
+# directive set /myproject/layer4/core/SoftmaxLatencyCalc -UNROLL yes
 
+# # Set II/RF for specific layers/blocks here. PIPELINE_INIT_INTERVAL = X means
+# # II = RF = X. Each layer/block may have a different value of II/RF. The II/RF
+# # of the entire design is the maximum PIPELINE_INIT_INTERVAL amongst the blocks.
+# directive set /myproject/layer1/core/main -PIPELINE_INIT_INTERVAL 1
+# directive set /myproject/layer2/core/main -PIPELINE_INIT_INTERVAL 1
+# directive set /myproject/layer3/core/main -PIPELINE_INIT_INTERVAL 1
+# directive set /myproject/layer4/core/main -PIPELINE_INIT_INTERVAL 1
+
+# directive set /myproject/layer2_out:cns -FIFO_DEPTH 0
+# directive set /myproject/layer3_out:cns -FIFO_DEPTH 0
+# directive set /myproject/layer4_out:cns -FIFO_DEPTH 0
+
+# directive set /myproject/layer4/core/main -PIPELINE_STALL_MODE bubble
+### Region (*) ends
+
+
+# Uncomment the region below (**) and comment the region beforehand (**) if you
+# want to synthesise Dense019 as an entire model (i.e. without using any blocks).
+# You can set II/RF through PIPELINE_INIT_INTERVAL (see the comment inside the
+# (**) region).
+
+
+### Region (**) starts
+# # Set II/RF for the entire model here. PIPELINE_INIT_INTERVAL = X means
+# # II = RF = X.
 # directive set /myproject/core/main -PIPELINE_INIT_INTERVAL 1
+
 # directive set /myproject/core/Product1 -UNROLL yes
 # directive set /myproject/core/Product2 -UNROLL yes
 # directive set /myproject/core/Accum1 -UNROLL yes
@@ -48,3 +70,7 @@ directive set /myproject/layer4/core/main -PIPELINE_STALL_MODE bubble
 # directive set /myproject/core/Result#1 -UNROLL yes
 # directive set /myproject/core/SoftmaxLatencyExp -UNROLL yes
 # directive set /myproject/core/SoftmaxLatencyCalc -UNROLL yes
+### Region (**) ends
+
+
+# DO NOT LEAVE BOTH REGIONS UNCOMMENTED!
